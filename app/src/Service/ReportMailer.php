4575d9487aaf989f94c3f5c671ab336a458ac262
daniel.palm <daniel.palm@immensegroup.com>
2025-11-30 21:58:41 +0100

    done

--- app/src/Service/ReportMailer.php
+++ app/src/Service/ReportMailer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Service;
 
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 
@@ -12,13 +13,18 @@ class ReportMailer
 {
     public function __construct(
         private readonly MailerInterface $mailer,
-        #[Autowire('%env(REPORT_EMAIL)%')] private readonly string $recipient
+        #[Autowire('%env(REPORT_EMAIL)%')] private readonly string $recipient,
+        #[Autowire('%env(REPORT_FROM_EMAIL)%')] private readonly string $fromEmail,
     ) {
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     public function sendReport(string $pdfContent): void
     {
         $email = (new Email())
+            ->from($this->fromEmail)
             ->to($this->recipient)
             ->subject('Weekly Feedback Report')
             ->text('Attached is the weekly feedback report in PDF format.')
