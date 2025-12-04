89b9a7de0c464fd224eb66ce0b8065b46935f416
DanielPalmSie <48477698+DanielPalmSie@users.noreply.github.com>
2025-12-04 19:55:55 +0100

    Configure Sentry bundle and logging

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
