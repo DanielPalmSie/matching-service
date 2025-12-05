5fdb618915a31559b2e3de30cf6b385390af2e89
daniel.palm <daniel.palm@immensegroup.com>
2025-12-05 20:03:30 +0100

    done

--- app/src/Controller/SentryTestController.php
+++ app/src/Controller/SentryTestController.php
@@ -2,23 +2,15 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SentryTestController extends AbstractController
 {
-    public function __construct(private LoggerInterface $logger)
-    {
-    }
-
     #[Route('/_sentry-test', name: 'sentry_test')]
-    public function testLog(): void
+    public function testLog(): Response
     {
-        // Проверка, что monolog шлёт в Sentry
-        $this->logger->error('My custom logged error.', ['some' => 'Context Data']);
-
-        // Проверка, что необработанные исключения тоже летят в Sentry
-        throw new \RuntimeException('Example exception.');
+        throw new \RuntimeException('PROD controller Sentry test');
     }
 }
