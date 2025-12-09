144f51fe2c1e45da02091e7bd3e58bd65d791f5f
DanielPalmSie <48477698+DanielPalmSie@users.noreply.github.com>
2025-12-09 14:24:00 +0100

    Publish Mercure event on magic login

--- app/src/Controller/Auth/MagicLoginPageController.php
+++ app/src/Controller/Auth/MagicLoginPageController.php
@@ -9,8 +9,12 @@ use App\Repository\MagicLoginTokenRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MagicLoginPageController extends AbstractController
@@ -19,6 +23,9 @@ class MagicLoginPageController extends AbstractController
         private readonly MagicLoginTokenRepository $magicLoginTokenRepository,
         private readonly EntityManagerInterface $entityManager,
         private readonly JWTTokenManagerInterface $jwtTokenManager,
+        private readonly HubInterface $hub,
+        #[Autowire(service: 'monolog.logger.magic_login')]
+        private readonly LoggerInterface $magicLoginLogger,
     ) {
     }
 
@@ -35,6 +42,22 @@ class MagicLoginPageController extends AbstractController
         $this->entityManager->flush();
 
         $jwt = $this->jwtTokenManager->create($magicToken->getUser());
+
+        $telegramChatId = $magicToken->getTelegramChatId();
+
+        if ($telegramChatId !== null) {
+            $topic = sprintf('/tg/login/%s', (string) $telegramChatId);
+            $payload = [
+                'type' => 'login_success',
+                'jwt' => $jwt,
+            ];
+
+            $this->hub->publish(new Update($topic, json_encode($payload, JSON_THROW_ON_ERROR)));
+
+            $this->magicLoginLogger->info('Sent Mercure login event for Telegram', [
+                'chat_id' => (string) $telegramChatId,
+            ]);
+        }
 
         return $this->render('auth/magic_login_success.html.twig', [
             'jwt' => $jwt,
